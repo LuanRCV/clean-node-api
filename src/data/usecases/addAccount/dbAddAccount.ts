84092045b887017dd56740116d6e2b1acb5504a9
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-07-24 16:11:35 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecases/addAccount/dbAddAccount.ts
+++ src/data/usecases/addAccount/dbAddAccount.ts
@@ -13,14 +13,8 @@ export class DbAddAccount implements AddAccount {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
     accountData.password = hashedPassword
 
-    await this.addAccountRepository.add(accountData)
-    return await new Promise(resolve => {
-      resolve({
-        id: 'valid_id',
-        name: 'valid_name',
-        email: 'valid_email',
-        password: 'valid_password'
-      })
-    })
+    const account = await this.addAccountRepository.add(accountData)
+
+    return account
   }
 }
