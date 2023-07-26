4740c229ab6b16c77f0d07f948f1aa6ff5828b56
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-07-26 16:52:59 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -0,0 +1,16 @@
+import { type AddAccountRepository } from '../../../../data/protocols/addAccountRepository'
+import { type AccountModel } from '../../../../domain/models/account'
+import { type AddAccountModel } from '../../../../domain/usecases/addAccount'
+import { MongoHelper } from '../helpers/mongo'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+
+    const account = result.ops[0]
+    const { _id, ...accountWithoutId } = account
+
+    return Object.assign({}, accountWithoutId, { id: _id })
+  }
+}
--- src/infra/db/mongodb/helpers/mongo.ts
+++ src/infra/db/mongodb/helpers/mongo.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, type Collection } from 'mongodb'
 
 export class MongoHelper {
   static client: MongoClient
@@ -9,5 +9,9 @@ export class MongoHelper {
 
   static async disconnect (): Promise<void> {
     await this.client.close()
+  }
+
+  static getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
