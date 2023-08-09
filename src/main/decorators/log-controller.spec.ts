73199d2472dc56105ccecb4a72541f6500b7099f
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-08-09 14:50:25 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log-controller.spec.ts
+++ src/main/decorators/log-controller.spec.ts
@@ -13,10 +13,7 @@ const makeController = (): Controller => {
         resolve({
           statusCode: 200,
           body: {
-            name: 'any_name',
-            email: 'any_mail@mail.com',
-            password: 'any_password',
-            passwordConfirmation: 'any_password'
+            name: 'any_name'
           }
         })
       })
@@ -54,5 +51,26 @@ describe('Log Controller Decorator', () => {
     await sut.handle(httpRequest)
 
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'any_name'
+      }
+    })
   })
 })
