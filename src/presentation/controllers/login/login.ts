8b122d7d112d3afa82ce86c60eaafc83cce7a329
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-08-14 11:45:50 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -10,25 +10,30 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['email', 'password']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return HttpHelper.badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return HttpHelper.badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const { email } = httpRequest.body
+      const { email } = httpRequest.body
 
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return HttpHelper.badRequest(new InvalidParamError('email'))
-    }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return HttpHelper.badRequest(new InvalidParamError('email'))
+      }
 
-    return await new Promise(resolve => {
-      resolve({
-        statusCode: 200,
-        body: {}
+      return await new Promise(resolve => {
+        resolve({
+          statusCode: 200,
+          body: {}
+        })
       })
-    })
+    } catch (error) {
+      console.error(error)
+      return HttpHelper.serverError(error)
+    }
   }
 }
