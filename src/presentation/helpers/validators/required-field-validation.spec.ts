f1de6536ed5d8ec2023a1f522a4eb378421cf58c
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-09-18 08:39:19 -0300

    test: ensure RequiredFieldValidation returns null if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -6,10 +6,23 @@ describe('RequiredField Validation', () => {
     return new RequiredFieldValidation('any_field')
   }
 
+  const makeFakeInput = (): any => {
+    return {
+      any_field: 'any_value'
+    }
+  }
+
   test('Should return a MissingParamError if validation fails', () => {
     const sut = makeSut()
     const validationError = sut.validate({})
 
     expect(validationError).toEqual(new MissingParamError('any_field'))
+  })
+
+  test('Should return null if validation succeeds', () => {
+    const sut = makeSut()
+    const validationError = sut.validate(makeFakeInput())
+
+    expect(validationError).toBeFalsy()
   })
 })
