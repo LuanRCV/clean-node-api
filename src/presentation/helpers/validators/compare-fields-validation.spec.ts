f76044cb0f58f193b671510bd53e3618312c02f2
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-09-18 08:52:29 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,32 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+describe('CompareFields Validation', () => {
+  const makeSut = (): CompareFieldsValidation => {
+    return new CompareFieldsValidation('any_field', 'any_field_to_compare')
+  }
+
+  const makeFakeInput = (): any => {
+    return {
+      any_field: 'any_value',
+      any_field_to_compare: 'any_value'
+    }
+  }
+
+  test('Should return an InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const validationError = sut.validate({
+      any_field: 'any_value',
+      any_field_to_compare: 'any_other_value'
+    })
+
+    expect(validationError).toEqual(new InvalidParamError('any_field_to_compare'))
+  })
+
+  test('Should return null if validation succeeds', () => {
+    const sut = makeSut()
+    const validationError = sut.validate(makeFakeInput())
+
+    expect(validationError).toBeFalsy()
+  })
+})
