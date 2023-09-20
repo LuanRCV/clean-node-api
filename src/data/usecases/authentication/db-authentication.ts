89f468502ede03c954089ba082da9987ddf1f80d
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-09-20 09:50:37 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,25 +1,26 @@
 import { type CredentialModel } from '../../../domain/models/credential'
 import { type AuthenticationModel, type Authentication } from '../../../domain/usecases/authentication'
+import { type HashComparer } from '../../protocols/criptography/hash-comparer'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly hashComparer: HashComparer
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashComparer = hashComparer
   }
 
   async auth (authentication: AuthenticationModel): Promise<CredentialModel | null> {
-    const { email } = authentication
+    const { email, password } = authentication
 
     const account = await this.loadAccountByEmailRepository.load(email)
 
-    if (!account) {
-      return null
+    if (account) {
+      await this.hashComparer.compare(password, account.password)
     }
 
-    return {
-      accessToken: 'any_access_token'
-    }
+    return null
   }
 }
