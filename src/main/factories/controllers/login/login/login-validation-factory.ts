9d6e32169b8e8ceda0a134fde40e85faeb9128ff
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-09-30 15:05:14 -0300

    refactor: move controllers factories to specific folders

---
+++ src/main/factories/controllers/login/login/login-validation-factory.ts
@@ -0,0 +1,14 @@
+import { EmailValidatorAdapter } from '../../../../../infra/validators/email-validator-adapter'
+import { type Validation } from '../../../../../presentation/protocols'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../../validation/validators'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  const requiredFields = ['email', 'password']
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+
+  return new ValidationComposite(validations)
+}
