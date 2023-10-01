9a6600d93899a111489dbd608ef74ae946714376
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-01 12:45:22 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct accessToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,16 +1,49 @@
 import { AuthMiddleware } from './auth-middleware'
 import { HttpHelper } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
+import { type HttpRequest } from '../protocols'
+import { type AccountModel } from '../../domain/models/account'
+import { type LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+
+const makeFakeAccount = (): AccountModel => {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_hashed_password'
+  }
+}
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return await new Promise(resolve => { resolve(makeFakeAccount()) })
+    }
+  }
+
+  return new LoadAccountByTokenStub()
+}
+
+const makeFakeRequest = (): HttpRequest => {
+  return {
+    headers: {
+      'x-access-token': 'any_access_token'
+    }
+  }
+}
 
 interface SutTypes {
   sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware()
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
 
   return {
-    sut
+    sut,
+    loadAccountByTokenStub
   }
 }
 
@@ -20,5 +53,14 @@ describe('Auth Middleware', () => {
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(HttpHelper.forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const fakeRequest = makeFakeRequest()
+    await sut.handle(fakeRequest)
+
+    expect(loadSpy).toHaveBeenCalledWith('any_access_token')
   })
 })
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,22 @@
+import { type LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
 import { HttpHelper } from '../helpers/http/http-helper'
 import { type Middleware, type HttpRequest, type HttpResponse } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken
+  ) { }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.headers?.['x-access-token']
+
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken)
+
+      return HttpHelper.ok({})
+    }
+
     return HttpHelper.forbidden(new AccessDeniedError())
   }
 }
