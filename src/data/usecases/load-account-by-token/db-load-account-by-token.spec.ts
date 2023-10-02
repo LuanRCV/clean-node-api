1e3e0c7aee76b0ecb8268aec71862cf7fdf78c3b
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-02 09:49:20 -0300

    test: ensure DbLoadAccountByToken throws if Decrypter throws

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -34,4 +34,12 @@ describe('DbLoadAccountByToken Usecase', () => {
 
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
   })
+
+  test('Should throw if Decrypter throws', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.load('any_token')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
