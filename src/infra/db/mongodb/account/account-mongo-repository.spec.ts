002d6947cd645bbcd99abc4a85fecfd36abe370f
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-02 10:42:48 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -68,6 +68,23 @@ describe('Account Mongo Repository', () => {
     expect(account).toBeFalsy()
   })
 
+  test('Should return an account on loadById success', async () => {
+    const sut = makeSut()
+    const result = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const newAccount = result.ops[0]
+    const account = await sut.loadById(newAccount._id)
+
+    expect(account).toBeTruthy()
+    expect(account?.id).toBeTruthy()
+    expect(account?.name).toBe('any_name')
+    expect(account?.email).toBe('any_email@mail.com')
+    expect(account?.password).toBe('any_password')
+  })
+
   test('Should update the account accessToken on updateAccessToken success', async () => {
     const sut = makeSut()
     const result = await accountCollection.insertOne({
