3ead67596eaeeeea41409064c7316bac1f1f6fa1
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-03 16:09:57 -0300

    test: ensure DbListSurveys calls ListSurveysRepository

---
+++ src/data/usecases/list-surveys/db-list-surveys.spec.ts
@@ -0,0 +1,58 @@
+import { type SurveyModel, type ListSurveysRepository } from './db-list-surveys-protocols'
+import { DbListSurveys } from './db-list-surveys'
+
+const makeFakeSurvey = (): SurveyModel => {
+  return {
+    id: 'any_id',
+    question: 'any_question',
+    date: new Date(),
+    answers: [
+      {
+        image: 'any_image_url_1',
+        text: 'any_text_1'
+      },
+      {
+        text: 'any_text_2'
+      }
+    ]
+  }
+}
+
+const makeListSurveysRepository = (): ListSurveysRepository => {
+  class ListSurveysRepositoryStub implements ListSurveysRepository {
+    async list (): Promise<SurveyModel[]> {
+      return await new Promise(resolve => { resolve([makeFakeSurvey()]) })
+    }
+  }
+
+  return new ListSurveysRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbListSurveys
+  listSurveysRepositoryStub: ListSurveysRepository
+}
+
+const makeSut = (): SutTypes => {
+  const listSurveysRepositoryStub = makeListSurveysRepository()
+  const sut = new DbListSurveys(listSurveysRepositoryStub)
+
+  return {
+    sut,
+    listSurveysRepositoryStub
+  }
+}
+
+describe('DbListSurveys Usecase', () => {
+  describe('Method list', () => {
+    describe('ListSurveysRepository integration', () => {
+      test('Should call list', async () => {
+        const { sut, listSurveysRepositoryStub } = makeSut()
+        const listSpy = jest.spyOn(listSurveysRepositoryStub, 'list')
+        await sut.list()
+
+        expect(listSpy).toHaveBeenCalled()
+      })
+    })
+  })
+})
