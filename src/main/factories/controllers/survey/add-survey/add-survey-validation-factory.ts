916beb7e3aa599f04ee5c7d0e11fd51d2ad11312
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-04 12:56:59 -0300

    refactor: add paths on main layer

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
@@ -1,5 +1,5 @@
-import { type Validation } from '../../../../../presentation/protocols/validation'
-import { ValidationComposite, RequiredFieldValidation } from '../../../../../validation/validators'
+import { type Validation } from '@presentation/protocols/validation'
+import { ValidationComposite, RequiredFieldValidation } from '@validation/validators'
 
 export const makeAddSurveyValidation = (): ValidationComposite => {
   const validations: Validation[] = []
--- src/main/factories/controllers/survey/list-surveys/list-surveys-controller-factory.ts
+++ src/main/factories/controllers/survey/list-surveys/list-surveys-controller-factory.ts
@@ -1,5 +1,5 @@
-import { ListSurveysController } from '../../../../../presentation/controllers/survey/list-surveys/list-surveys-controller'
-import { type Controller } from '../../../../../presentation/protocols'
+import { ListSurveysController } from '@presentation/controllers/survey/list-surveys/list-surveys-controller'
+import { type Controller } from '@presentation/protocols'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
 import { makeDbListSurveys } from '../../../usecases/survey/list-surveys/db-list-surveys-factory'
 
