1217fd9bc43987566c1e9d3bc37118c0fd593da1
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-04 10:25:18 -0300

    fix: correct files reference

--- src/main/middlewares/admin-auth.ts
+++ src/main/middlewares/admin-auth.ts
@@ -1,4 +1,4 @@
-import { adaptMiddleware } from '../adapters/express-middleware-adapter.ts'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory.js'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
--- src/main/middlewares/auth.ts
+++ src/main/middlewares/auth.ts
@@ -1,4 +1,4 @@
-import { adaptMiddleware } from '../adapters/express-middleware-adapter.ts.js'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory.js'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export const auth = adaptMiddleware(makeAuthMiddleware())
