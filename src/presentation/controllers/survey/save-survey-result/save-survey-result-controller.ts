9013b686de827643b482dd9a15c4fe19c58e8c00
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-05 13:56:07 -0300

    feat: ensure SaveSurveyResultController calls LoadSurveyById usecase with correct id

--- src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
@@ -1,9 +1,11 @@
 import { type HttpRequest, type Controller, type HttpResponse, type Validation } from './save-survey-result-protocols'
 import { HttpHelper } from '../../../helpers/http/http-helper'
+import { type LoadSurveyById } from '@domain/usecases/load-survey-by-id'
 
 export class SaveSurveyResultController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly loadSurveyById: LoadSurveyById
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -13,6 +15,9 @@ export class SaveSurveyResultController implements Controller {
       if (validationError) {
         return HttpHelper.badRequest(validationError)
       }
+
+      const { surveyId } = httpRequest.params
+      await this.loadSurveyById.loadById(surveyId)
 
       return HttpHelper.noContent()
     } catch (error) {
