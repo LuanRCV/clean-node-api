4996cfb721a7be930e2370715e374813cd813639
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-05 16:13:17 -0300

    test: ensure SaveSurveyResultController returns 500 if SaveSurveyResult usecase throws

--- src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.spec.ts
@@ -169,4 +169,12 @@ describe('SaveSurveyResult Controller', () => {
 
     expect(saveSpy).toHaveBeenCalledWith(makeFakeSaveSurveyResult())
   })
+
+  test('Should return 500 if SaveSurveyResult throws', async () => {
+    const { sut, saveSurveyResultStub } = makeSut()
+    jest.spyOn(saveSurveyResultStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(HttpHelper.serverError(new ServerError()))
+  })
 })
