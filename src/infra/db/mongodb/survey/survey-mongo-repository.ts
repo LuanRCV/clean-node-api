d8a0cea533d8d804bae616eac853f4cc7d3a57a5
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-19 14:53:29 -0300

    feat: ensure SurveyMongoRepository.loadById returns null on fail

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,11 @@
 import { type AddSurveyRepository } from '@data/protocols/db/survey/add-survey-repository'
-import { type SurveyModel, type ListSurveysRepository } from '@data/usecases/list-surveys/db-list-surveys-protocols'
+import { type LoadSurveyByIdRepository } from '@data/usecases/load-survey-by-id/db-load-survey-by-id-protocols'
+import { type ListSurveysRepository } from '@data/usecases/list-surveys/db-list-surveys-protocols'
 import { type AddSurveyModel } from '@domain/usecases/add-survey'
+import { type SurveyModel } from '@domain/models/survey'
 import { MongoHelper } from '../helpers/mongo'
 
-export class SurveyMongoRepository implements AddSurveyRepository, ListSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, ListSurveysRepository, LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
@@ -14,5 +16,16 @@ export class SurveyMongoRepository implements AddSurveyRepository, ListSurveysRe
     const surveys = await surveyCollection.find().toArray()
 
     return surveys.map(survey => MongoHelper.mapEntity<SurveyModel>(survey))
+  }
+
+  async loadById (id: string): Promise<SurveyModel | null> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const survey = await surveyCollection.findOne({ _id: id })
+
+    if (survey) {
+      return survey
+    }
+
+    return null
   }
 }
