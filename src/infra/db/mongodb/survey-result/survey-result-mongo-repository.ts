7426f9e80bbd046d9ea2aa1019b3b01bb9215ec8
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-19 16:01:35 -0300

    feat: ensure SurveyResultMongoRepository add a SurveyResult if it is new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,27 @@
+import { type SaveSurveyResultModel, type SurveyResultModel, type SaveSurveyResultRepository } from '@data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { MongoHelper } from '../helpers/mongo'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (saveSurveyResultData: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const { surveyId, accountId, answer, date } = saveSurveyResultData
+    const result = await surveyResultCollection.findOneAndUpdate(
+      {
+        surveyId,
+        accountId
+      },
+      {
+        $set: {
+          answer,
+          date
+        }
+      },
+      {
+        upsert: true,
+        returnOriginal: false
+      }
+    )
+
+    return MongoHelper.mapEntity<SurveyResultModel>(result.value)
+  }
+}
