769a87f25379d9ced422129928f55da762c6633b
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-10-27 09:31:31 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,35 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Surveys API',
     description: 'API to manage Surveys and Accounts',
     version: '1.0.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    },
+    {
+      name: 'Survey'
+    },
+    {
+      name: 'Survey Result'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
