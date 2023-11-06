86beb1fba3b2eb1d6e398c390c6f470585a05b27
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-06 08:33:57 -0300

    test: ensure DbLoadSurveyResult throws if LoadSurveyResultRepository throws

--- src/data/usecases/survey-result/load-survey-result-by-survey-id/db-load-survey-result-by-survey-id.spec.ts
+++ src/data/usecases/survey-result/load-survey-result-by-survey-id/db-load-survey-result-by-survey-id.spec.ts
@@ -1,6 +1,7 @@
 import { type LoadSurveyResultBySurveyIdRepository } from './db-load-survey-result-by-survey-id-protocols'
 import { DbLoadSurveyResultBySurveyId } from './db-load-survey-result-by-survey-id'
 import { mockLoadSurveyResultBySurveyIdRepository } from '@data/test/mock-db-survey-result'
+import { throwError } from '@domain/test'
 
 type SutTypes = {
   sut: DbLoadSurveyResultBySurveyId
@@ -33,6 +34,14 @@ describe('DbLoadSurveyResultBySurveyId Usecase', () => {
       const surveyResult = await sut.load('any_id')
 
       expect(surveyResult).toBeNull()
+    })
+
+    test('Should throw if loadBySurveyId throws', async () => {
+      const { sut, loadSurveyResultRepositoryStub } = buildSut()
+      jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
+      const promise = sut.load('any_id')
+
+      await expect(promise).rejects.toThrow()
     })
   })
 })
